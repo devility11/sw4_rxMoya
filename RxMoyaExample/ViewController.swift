b0dfbbb9e53f7907b51f7426b05deca837af0bea
Norbert Czirjak <czirjak.norbert@gmail.com>
2017-10-29 14:01:08 +0100

    endpoint added

--- RxMoyaExample/ViewController.swift
+++ RxMoyaExample/ViewController.swift
@@ -7,18 +7,29 @@
 //
 
 import UIKit
+import Moya
+import Moya_ModelMapper
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let disposebag = DisposeBag()
+    var provider: RxMoyaProvider<GitHub>!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupRx()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupRx(){
+        
     }
+    
 
 
 }
